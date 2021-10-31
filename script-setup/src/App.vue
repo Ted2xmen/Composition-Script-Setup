cf43f02faee9d4166f36e773d5f62d332461732f
Ted <tugrulerdemdogru@gmail.com>
2021-10-31 22:57:06 +0300

    composition exe

---
+++ script-setup/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>{{title}}</h3>
+  <input type="text" v-model="title">
+  <button @click="inc">{{counter}} arttır</button>
+  <hr>
+  <OddOrEven :counter="counter" @odd-event="alertMe" />
+</template>
+
+
+
+<script setup> // beforeCreate created ve data yerine geçiyor setup
+import {ref} from "vue"
+import OddOrEven from "./components/OddOrEven.vue"
+import Utils from "./composables/Utils"
+
+
+// composables'A taşındı
+  // const title = ref("Başlık")
+  // const counter = ref(0)
+  // const inc = () => {
+  //   counter.value++
+  // }
+  // const alertMe = (info) => {
+  //   console.log(info);
+  // }
+
+  const {title, counter, inc, alertMe} = Utils();
+
+</script>
---
+++ script-setup/src/components/OddOrEven.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>D</h1>
+    <p>{{ counter }}</p>
+    <p>{{ result }}</p>
+</template>
+
+
+
+<script setup>
+import {computed, watch} from "vue"
+
+
+const props = defineProps({counter: Number})
+const emit = defineEmits()
+const result = computed(() => props.counter % 2 === 0 ? 'çift' : 'tek')
+
+watch(result, (result) => {
+    if(result === 'tek') emit("odd-event", true)
+})
+
+</script>
